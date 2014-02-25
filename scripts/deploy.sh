8285d13f0645b30771d31c3ff94667ca306050dc
Scott Ellis <scott@jumpnowtek.com>
2014-02-25 14:11:22 -0500

    Add jekyll build to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd ..
+jekyll build
 sudo /etc/init.d/nginx stop
 sudo rm -r /var/www/*
-sudo cp -r ../_site/* /var/www
+sudo cp -r _site/* /var/www
 sudo /etc/init.d/nginx start
+cd $OLDPWD  
 
