edc84c8cbd3fa43cfb1ad46725a209be99cef198
Scott Ellis <scott@jumpnowtek.com>
2020-05-17 12:06:25 -0400

    prod-deploy: Tighten permissions

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -5,7 +5,7 @@ sudo chmod 755 /var/www
 sudo rm -rf /var/www/*
 sudo tar -C /var/www --strip 1 -xzf site.tgz
 sudo chown -R www-data:www-data /var/www
-sudo find /var/www -type d -perm 775 -print -exec chmod 555 {} \;
+sudo find /var/www -type d -print -exec chmod 555 {} \;
 sudo find /var/www -type f -print -exec chmod 444 {} \;
 sudo chmod 555 /var/www
 sudo systemctl start nginx
